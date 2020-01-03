b9a415c664fb921d564a93a7929399ac35850c40
何梓杰 <hezijie@glbdata.com>
2020-01-03 17:47:08 +0800

    更新测试工具

--- test/SimulatedExchange.Reporting.Test/MainWindow.xaml.cs
+++ test/SimulatedExchange.Reporting.Test/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
 using System;
 using System.Text.Json;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace SimulatedExchange.Reporting.Test
@@ -18,38 +19,44 @@ namespace SimulatedExchange.Reporting.Test
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var address = addressTextBox.Text;
-            if (string.IsNullOrWhiteSpace(address))
+            if (connection == null)
             {
-                PrintMessage("服务器地址不能为空");
-                return;
+                var address = addressTextBox.Text;
+                if (string.IsNullOrWhiteSpace(address))
+                {
+                    PrintMessage("服务器地址不能为空");
+                    return;
+                }
+                try
+                {
+                    connection = new HubConnectionBuilder()
+                    .WithUrl(address)
+                    .Build();
+
+
+                    connection.On<OrderState>("New", WriteToConsole);
+                    connection.On<OrderState>("PartialDeal", WriteToConsole);
+                    connection.On<OrderState>("FullDeal", WriteToConsole);
+                    connection.On<OrderState>("PartialCancel", WriteToConsole);
+                    connection.On<OrderState>("FullCanceled", WriteToConsole);
+
+                    connection.StartAsync();
+                }
+                catch (Exception ex)
+                {
+                    PrintMessage(ex.Message);
+                }
             }
-            try
-            {
-                connection = new HubConnectionBuilder()
-                .WithUrl(address)
-                .Build();
-
-
-                connection.On<OrderState>("New", WriteToConsole);
-                connection.On<OrderState>("PartialDeal", WriteToConsole);
-                connection.On<OrderState>("FullDeal", WriteToConsole);
-                connection.On<OrderState>("PartialCancel", WriteToConsole);
-                connection.On<OrderState>("FullCanceled", WriteToConsole);
-
-                connection.StartAsync();
-            }
-            catch (Exception ex)
-            {
-                PrintMessage(ex.Message);
-            }
-
+            PrintMessage("已建立链接");
         }
 
         protected override void OnClosed(EventArgs e)
         {
-            connection.StopAsync();
-            base.OnClosed(e);
+            connection.StopAsync().ContinueWith(task =>
+            {
+                base.OnClosed(e);
+                return Task.CompletedTask;
+            });
         }
 
         private void WriteToConsole(OrderState state) => PrintMessage(JsonSerializer.Serialize(state));
