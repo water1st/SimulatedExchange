817342fff09bc1b4717bc50cba96c0969636b199
何梓杰 <hezijie@glbdata.com>
2020-01-06 14:43:14 +0800

    sql脚本更新

--- src/SimulatedExchange.DataAccess.MySQL/Scripts/simulated_exchange_reporting_storage.sql
+++ src/SimulatedExchange.DataAccess.MySQL/Scripts/simulated_exchange_reporting_storage.sql
@@ -1,17 +1,17 @@
-﻿/*
+/*
  Navicat Premium Data Transfer
 
- Source Server         : 192.168.82.37
+ Source Server         : 192.168.0.244
  Source Server Type    : MySQL
  Source Server Version : 80015
- Source Host           : 192.168.82.37:3306
+ Source Host           : 192.168.0.244:3306
  Source Schema         : simulated_exchange_reporting_storage
 
  Target Server Type    : MySQL
  Target Server Version : 80015
  File Encoding         : 65001
 
- Date: 23/12/2019 15:23:17
+ Date: 06/01/2020 14:42:59
 */
 
 SET NAMES utf8mb4;
@@ -23,7 +23,6 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `orders`;
 CREATE TABLE `orders`  (
   `Id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `ClientId` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `FromCurrencySymbol` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
   `ToCurrencySymbol` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
   `Price` decimal(10, 0) NULL DEFAULT NULL,
@@ -34,8 +33,7 @@ CREATE TABLE `orders`  (
   `Status` int(11) NULL DEFAULT NULL,
   `CreatedTimeUtc` datetime(0) NULL DEFAULT NULL,
   `ModifyedTimeUtc` datetime(0) NULL DEFAULT NULL,
-  PRIMARY KEY (`Id`) USING BTREE,
-  UNIQUE INDEX `ORDER_CLIENTID_UNIQUE_INDEX`(`ClientId`) USING BTREE
+  PRIMARY KEY (`Id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
